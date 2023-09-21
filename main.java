f9dbc873c194215365b2424a7098b3d343d58060
hanoeyf <hanifahkurniasari0512@gmail.com>
2023-09-21 13:44:13 +0700

    save

---
+++ main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class main{
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+int jumlahBarang; 
+int barangSewa;
+int totalPembayaran;
+
+System.out.print("Masukkan Jumlah barang: ");
+jumlahBarang = sc14.nextInt();
+System.out.print("Masukkan Barang sewa: ");
+barangSewa = sc.nextInt();
+totalPembayaran = jumlahBarang * barangSewa ;
+System.out.println("total pembayaran: "+ totalPembayaran);
+
+}
+}

375f672eec5d5dcebcfe6c69f9892c46b13d86fa
soulqan <soultan.agnar@gmail.com>
2023-09-24 22:19:08 +0700

    test

--- main.java
+++ main.java
@@ -1,22 +1,4 @@
 import java.util.Scanner;
-<<<<<<< HEAD
-public class main{
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-int jumlahBarang; 
-int barangSewa;
-int totalPembayaran;
-
-System.out.print("Masukkan Jumlah barang: ");
-jumlahBarang = sc14.nextInt();
-System.out.print("Masukkan Barang sewa: ");
-barangSewa = sc.nextInt();
-totalPembayaran = jumlahBarang * barangSewa ;
-System.out.println("total pembayaran: "+ totalPembayaran);
-
-}
-=======
-
 public class main {
     public static int NUM_PRODUCTS = 10;
     public static String[] namaProduk = {"Tenda gunung", "Tas Gunung", "Sleeping Bag", "Kompor Portable", "Cookingset", "FlashLight ", "Karpet tebal", "product8", "Product9", "Product10"};

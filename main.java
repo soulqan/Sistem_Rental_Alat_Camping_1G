0bf62eec145c82370dba599b896630fcc32dbb90
fateehhhh <siyaapppp@gmail.com>
2023-11-03 13:28:54 +0700

    update main

--- main.java
+++ main.java
@@ -1,7 +1,163 @@
+import java.util.Scanner;
 public class main {
+    public static int NUM_PRODUCTS = 10;
+    public static String[] namaProduk = {"Tenda gunung", "Tas Gunung", "Sleeping Bag", "Kompor Portable", "Cookingset", "FlashLight", "Karpet tebal", "product8", "Product9", "Product10"};
+    public static int[] jmlBarang = { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+    public static long[] harga = {250000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000};
+    public static String[] itemKeranjang = new String[NUM_PRODUCTS];
+    public static int[] jmlKeranjang = new int[NUM_PRODUCTS];
+    public static long totalHarga = 0;
 
     public static void main(String[] args) {
-        
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=================================================");
+			System.out.println("  Selamat datang di sistem rental alat camping          ");
+                        System.out.println("=================================================");
+			System.out.print("masukan user: ");
+			String user =scanner.nextLine();
+			System.out.print("masukan password: ");
+			int password = scanner.nextInt();
+			
+			if( (user.equalsIgnoreCase("admin")) && (password==123) ){
+			
+			System.out.println("\n               Login Berhasil              ");
+			System.out.println("\n");
+
+
+            }
+        int pilihan;
+        do {
+            System.out.println("\n----- Menu Utama -----");
+            System.out.println("1. Persediaan Alat ");
+            System.out.println("2. Masukkan keranjang");
+            System.out.println("3. Buang dari Keranjang");
+            System.out.println("4. Rincian Pembelian");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            pilihan = scanner.nextInt();
+
+            switch (pilihan) {
+                case 1:
+                    Persediaan();
+                    break;
+                case 2:
+                    tambahkanProdukKeranjang(scanner);
+                    break;
+                case 3:
+                    hapusProdukDariKeranjang(scanner);
+                    break;
+                case 4:
+                    rincian();
+                    break;
+                case 5:
+                    System.out.println("Thank you for using Alat Rental Camping. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+        } while (pilihan != 5);
+
+        scanner.close();}
+    
+
+    // Method untuk menampilkan pesanan
+    public static void Persediaan() {
+        System.out.println("\n----- Produk -----");
+        System.out.println("Nama\t\tTersedia\tHarga");
+
+        for (int i = 0; i < NUM_PRODUCTS; i++) {
+            System.out.print(namaProduk[i] + "\t");
+            if (jmlBarang[i] == 0) {
+                System.out.print("Sold out\t");
+            } else {
+                System.out.print(jmlBarang[i] + "\t\t");
+            }
+            System.out.println(harga[i]);
+        }
+    }
+
+    // method untuk menambahkan produk ke keranjang
+    public static void tambahkanProdukKeranjang(Scanner scanner) {
+        System.out.println("\n----- Add Product to Cart -----");
+        System.out.print("Enter the product name: ");
+        String Produk = scanner.nextLine();
+        Produk = scanner.nextLine();
+
+        int productIndex = -1;
+        for (int i = 0; i < NUM_PRODUCTS; i++) {
+            if (namaProduk[i].equalsIgnoreCase(Produk)) {
+                productIndex = i;
+                break;
+            }
+        }
+
+        if (productIndex == -1) {
+            System.out.println("Produk tidak ditemukan.");
+        } else if (jmlBarang[productIndex] == 0) {
+            System.out.println("Sold Out.");
+        } else {
+            System.out.print("Masukkan jumlah barang: ");
+            int jumlah = scanner.nextInt();
+
+            if (jumlah > jmlBarang[productIndex]) {
+                System.out.println("Jumlah yang tersedia tidak mencukupi.");
+            } else {
+                itemKeranjang[productIndex] = Produk;
+                jmlKeranjang[productIndex] += jumlah;
+                totalHarga += harga[productIndex] * jumlah;
+
+                jmlBarang[productIndex] -= jumlah;
+                System.out.println("Produk ditambahkan ke keranjang.");
+            }
+        }
+    }
+
+    //method untuk menghapus barang yang ada di keranjang
+    public static void hapusProdukDariKeranjang(Scanner scanner) {
+        System.out.println("\n----- Remove Product from Cart -----");
+        System.out.print("Enter the product name: ");
+        String productName = scanner.next();
+
+        int productIndex = -1;
+        for (int i = 0; i < NUM_PRODUCTS; i++) {
+            if (itemKeranjang[i] != null && itemKeranjang[i].equalsIgnoreCase(productName)) {
+                productIndex = i;
+                break;
+            }
+        }
+
+        if (productIndex == -1) {
+            System.out.println("Product not found in cart.");
+        } else {
+            System.out.print("Enter the quantity: ");
+            int jumlah = scanner.nextInt();
+
+            if (jumlah > jmlKeranjang[productIndex]) {
+                System.out.println("Jumlah tidak valid.");
+            } else {
+                jmlKeranjang[productIndex] -= jumlah;
+                totalHarga -= harga[productIndex] * jumlah;
+
+                jmlBarang[productIndex] += jumlah;
+                System.out.println("Produk telah dibuang dari keranjang.");
+            }
+        }
+    }
+
+    // Method untuk menunjukkan total pembayaran dan barang apa saja yang disewa
+    public static void rincian() {
+        System.out.println("\n----- Cart Items -----");
+        System.out.println("Name\t\tQuantity\tPrice");
+
+        for (int i = 0; i < NUM_PRODUCTS; i++) {
+            if (itemKeranjang[i] != null) {
+                System.out.print(itemKeranjang[i] + "\t");
+                System.out.print(jmlKeranjang[i] + "\t\t");
+                System.out.println(harga[i]);
+                 System.out.println("Total Price: " + totalHarga);
+            }
+        }
+
     }
 }
